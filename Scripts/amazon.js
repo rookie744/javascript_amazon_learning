31d3298003a971afee4204b06f05e182530e1d6c
Kishore Kumar K <85016542+rookie744@users.noreply.github.com>
2025-07-10 22:57:10 +0530

    cart json july 10th

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-cart" data-productid = "${product.id}">
             Add to Cart
           </button>
     </div>`;
@@ -55,4 +55,35 @@ products.forEach((product) => {
 // console.log(productHTML);
 
 document.querySelector('.products-grid').innerHTML=productHTML;
+
+//add to cart functionality
+let add_cart_btn = document.querySelectorAll('.js-add-cart');
+
+add_cart_btn.forEach((button)=>
+{
+  button.addEventListener('click',() => {
+    let check_presence;
+    let productid = button.dataset.productid;
+    cart.forEach((product)=>
+    {
+      if(product.productid === productid)
+      {
+        product.quantity += 1;
+        check_presence = true;
+      }
+    })
+
+    if (check_presence === undefined)
+    {
+      cart.push(
+      {
+        productid:productid,
+        quantity : 1
+      }
+    )
+    }
+  // console.log(cart);
+  });
+});
+
 
